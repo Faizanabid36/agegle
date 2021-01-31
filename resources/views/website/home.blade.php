4fafa8c9a0de42566331cfef18ad5e3f7208f05f
muhammadzain940 <muhammad.zain.11920@gmail.com>
2021-01-31 20:29:15 +0500

    asd

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -1,19 +1,37 @@
 @extends('website_layouts.main')
 
 @section('content')
+
+<style>
+    .zoom {
+  transition: transform .8s;
+}
+    .zoom:hover {
+  -ms-transform: scale(1.5); /* IE 9 */
+  -webkit-transform: scale(1.5); /* Safari 3-8 */
+  transform: scale(1.2); 
+}
+@media only screen and (max-width: 500px) {
+.z{
+    height: 130px !important ;
+    width: 130px !important ;
+    margin-left: 0px !important;
+}
+}
+</style>
     <body>
     <div style="padding: 25px 0px">
         <div class="container" style="padding-bottom: 110px">
             @if(count($pages)>0)
                 <div class="row" id="content">
                     @foreach($pages as $page)
-                        <div class="col-xs-4 col-md-3">
+                        <div class="col-xs-6 col-md-3">
                             <a href="{{route('view',$page->slug)}}">
-                                <img style="height: 170px;width: 240px;"
+                                <img style="height: 170px;width: 240px;"  class="zoom z"
                                      src="{{$page->disp_img??$page->extra->first()->attachment_url}}"
                                      alt="{{$page->slug}}"
                                      class="img-responsive"></a>
-                            <div style="margin-left: 10px;">
+                            <div style="margin-left: 10px;" class="z">
                                 <h4 class="txt" style="color: black;margin: 10px 0 0 0">{{ucfirst($page->name)}}</h4>
                                 <p>Age {{$page->disp_age??$page->extra->count()}} of {{$page->extra->count()}} years</p>
                             </div>
